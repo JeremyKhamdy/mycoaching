c4f709a2770f217aca4f150548c9e70b85fc7e40
Khamdy <jeremy.khamdy@epitech.eu>
2025-06-04 13:18:45 +0200

    [ADD] Vitest + test on account store

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,24 @@
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
+    plugins: [vue()],
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url))
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      globals: true,
+      setupFiles: ['./vitest.setup.ts']
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
     }
   })
 )
